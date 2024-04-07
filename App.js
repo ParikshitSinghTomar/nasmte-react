e99569669ba629731c3c91a9a80129223dc5321e
Parikshit Singh Tomar <parikshitsinghtomar1857@gmail.com>
2024-04-07 17:08:08 +0530

    Adding Multiple React generated elements

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
-const heading=React.createElement("h1",{id:"heading",xyz:"abc"},"Hello World from React");
+const heading = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello World from React");
+console.log(heading);//will not print any html code, it will print react code.
 
-console.log(heading);
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+const parent = React.createElement("div", { id: "parent" },
+    [React.createElement("div", { id: "child" },
+        [React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h1", {}, "I'm an h2 tag")]),
+    React.createElement("div", { id: "child2" },
+        [React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h1", {}, "I'm an h2 tag")])])
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent)
