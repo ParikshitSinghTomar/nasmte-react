d0fd0dd7113a9d3a668c9d6a3012d511212df101
Parikshit Singh Tomar <parikshitsinghtomar1857@gmail.com>
2024-05-04 15:14:52 +0530

    Print using JSX code

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello World from React");
-console.log(heading);//will not print any html code, it will print react code.
+// const heading = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello World from React");
+// console.log(heading);//will not print any html code, it will print react code.
 
-const parent = React.createElement("div", { id: "parent" },
-    [React.createElement("div", { id: "child" },
-        [React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h1", {}, "I'm an h2 tag")]),
-    React.createElement("div", { id: "child2" },
-        [React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h1", {}, "I'm an h2 tag")])])
+// const parent = React.createElement("div", { id: "parent" },
+//     [React.createElement("div", { id: "child" },
+//         [React.createElement("h1", {}, "I'm an h1 tag"),
+//         React.createElement("h1", {}, "I'm an h2 tag")]),
+//     React.createElement("div", { id: "child2" },
+//         [React.createElement("h1", {}, "I'm an h1 tag"),
+//         React.createElement("h1", {}, "I'm an h2 tag")])])
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent)
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent)
+
+const jsxheading=<h1 id="heading">Namste React using JSX</h1>
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxheading);
